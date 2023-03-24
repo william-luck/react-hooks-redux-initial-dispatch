663623516917433f2420077f1af924f57d911376
William Luck <luck.william.h@gmail.com>
2023-03-24 10:02:32 -0300

    commit tree 015f652ed9822e2670ef608125c6f1c053ed5ab3

--- src/reducer.js
+++ src/reducer.js
@@ -1,6 +1,7 @@
-let state = { count: 0 };
+let state;
 
-function changeState(state, action) {
+// The difference of this lab is we declare but never assign state, and assign a default value for state, because it will be undefined with the init dispatch. 
+function changeState(state = {count: 0}, action) {
   switch (action.type) {
     case "counter/increment":
       return { count: state.count + 1 };
@@ -18,3 +19,6 @@ function render() {
   const app = document.querySelector("#app");
   app.textContent = state.count;
 }
+
+// Responsible for the initial render on the page, because it will attempt to change state, only being returned the default value from the reducer
+dispatch({ type: "@@INIT" });
